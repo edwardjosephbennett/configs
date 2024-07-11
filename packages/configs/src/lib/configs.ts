dc48f6d4c085cabdebf9e11ac5a9e608c2b9637d
Ed Bennett <6391276+edwardjosephbennett@users.noreply.github.com>
2024-07-11 21:33:44 +0100

    feat(configs): some new debugs

--- packages/configs/src/lib/configs.ts
+++ packages/configs/src/lib/configs.ts
@@ -1,5 +1,7 @@
 export function configs(): string {
   console.log("Hello World from configs!")
   console.log("Hello World from configs NEW!")
+  console.log("Hello World from configs NEW 2!")
+  console.log("Hello World from configs NEW 3!")
   return 'configs';
 }
