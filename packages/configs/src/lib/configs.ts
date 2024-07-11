1ac1080c07a49973db6fab5f003b8e54b7580835
Ed Bennett <6391276+edwardjosephbennett@users.noreply.github.com>
2024-07-11 21:40:16 +0100

    feat(configs): add a default export

--- packages/configs/src/lib/configs.ts
+++ packages/configs/src/lib/configs.ts
@@ -1,7 +1,5 @@
 export function configs(): string {
-  console.log("Hello World from configs!")
-  console.log("Hello World from configs NEW!")
-  console.log("Hello World from configs NEW 2!")
-  console.log("Hello World from configs NEW 3!")
   return 'configs';
 }
+
+export default configs;
