14b708bc380e47174b9244327b5fc692eb1e7256
Ed Bennett <6391276+edwardjosephbennett@users.noreply.github.com>
2024-10-01 14:18:16 +0100

    feature(functions): add some new exports

--- packages/configs/src/lib/configs.ts
+++ packages/configs/src/lib/configs.ts
@@ -8,4 +8,14 @@ export function edsFancyOutput(param: string) : null {
   return null;
 }
 
+export function anotherFancyOutput(name: string) : null {
+  console.warn(`Hello, ${name}`);
+  return null;
+}
+
+export function addition(a: number, b: number) : number {
+  console.log('From Addition:', a + b);
+  return a + b;
+}
+
 export default consoleOutput;
